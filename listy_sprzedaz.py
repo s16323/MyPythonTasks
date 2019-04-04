1fef074fcb496799f44df6c548166e05e3f06ba6
Artur Dembicki <artur.dembicki@gmail.com>
2019-04-04 10:47:22 +0200

    add listy_sprzedaz.py

---
+++ listy_sprzedaz.py
@@ -0,0 +1,15 @@
+'''
+Przykład. Dane są cztery listy: r1, r2, r3 i r4 liczące po 12 elementów. Zawierają one wartości miesięcznej sprzedaży przez kolejne cztery lata w pewnej firmie. Oblicz:
+
+1.     Sumę sprzedaży dla poszczególnych lat
+
+2.     Sumę sprzedaży w poszczególnych miesiącach przez cztery lata
+
+3.     Średnią sprzedaż miesięczną przez cztery lata
+'''
+
+# Rozwiązanie:
+# Na początek przygotujemy losowe dane wejściowe:
+
+from random import *
+help(seed)
